200835364ed80eb247d3f5d2b71fe5318caa32fb
Kee Tee <tkeeching@gmail.com>
2020-07-20 16:49:44 +1000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useRef, useEffect } from 'react';
+import { 
+  Switch, 
+  Route,
+  useParams 
+} from "react-router-dom";
 import './App.css';
+import { TweenMax, TimelineLite, Power3 } from 'gsap';
+
+function Name() {
+  const { name } = useParams();
+  let line0 = useRef(null);
+  let tl0 = new TimelineLite({ delay: 1 })
+
+  useEffect(() => {
+    tl0.from(line0, 1.3, {y: 2000, ease: Power3.easeOut})
+  })
+
+  return <h1 ref={el => line0 = el}>Hello {name},</h1>
+}
 
 function App() {
+  let container = useRef(null);
+  let line1 = useRef(null);
+  let line2 = useRef(null);
+  let line3 = useRef(null);
+  let tl = new TimelineLite({ delay: 2 })
+
+  useEffect(() => {
+    TweenMax.to(container, 0, {css: {visibility: "visible"}})
+    tl.from(line1, 1.3, {x: -2000, ease: Power3.easeOut})
+      .from(line2, 1.3, {x: 2000, ease: Power3.easeOut})
+      .from(line3, 1.8, {y: -2000, scale: 6, ease: Power3.easeOut});
+  })
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App pyro" ref={el => container = el}>
+      <Switch>
+        <Route path="/:name">
+          <Name />
+          <h1 ref={el => line1 = el}>It's</h1>
+          <h1 ref={el => line2 = el}>your</h1>
+          <h1 ref={el => line3 = el}>birthday!</h1>
+          <div className="before"></div>
+          <div className="after"></div>
+        </Route> 
+        <Route path="/">
+          <h1 ref={el => line1 = el}>It's</h1>
+          <h1 ref={el => line2 = el}>your</h1>
+          <h1 ref={el => line3 = el}>birthday!</h1>
+          <div className="before"></div>
+          <div className="after"></div>
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
